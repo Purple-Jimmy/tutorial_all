37173cd100ad787341e75e5644c39c0d1a5a8461
jimmy <773792867@qq.com>
2019-04-15 11:44:26 +0800

    tutorial

--- tutorial_common/src/main/java/com/tutorial/entity/Account.java
+++ tutorial_common/src/main/java/com/tutorial/entity/Account.java
@@ -1,7 +1,10 @@
 package com.tutorial.entity;
 
-import lombok.Data;
+import lombok.*;
+import lombok.experimental.Accessors;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -9,10 +12,28 @@ import java.io.Serializable;
  * @Date: 2019/4/15
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@Table
+@Entity(name="account")
 public class Account implements Serializable {
     private static final long serialVersionUID = -2897227658999048216L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String name;
+    @Size(min=5,max=10,message = "名字长度5-10位")
+    private String userName;
+
+    //@Size(min=6,max=10)
+    //@CustomValidator(values = "1,2,3")
+    private String password;
+
+    private String email;
+
+    private String token;
 }
